b1fca746daa338623c9a7e85eb789d368fd993db
Yo_Andy <8191416+sddyd@user.noreply.gitee.com>
2023-10-31 03:09:11 +0000

    js
    Signed-off-by: Yo_Andy <8191416+sddyd@user.noreply.gitee.com>

---
+++ js/index.js
@@ -0,0 +1,87 @@
+function slideSwitch() {
+    var $active = $('#slideshow img.active');
+    if ( $active.length == 0 ) $active = $('#slideshow img:last');
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow img:first');
+  
+    var $sibs  = $active.siblings();
+    var rndNum = Math.floor(Math.random() * $sibs.length );
+    var $next  = $( $sibs[ rndNum ] );
+    $active.addClass('last-active');
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 2000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+$(function() {
+      setInterval( "slideSwitch()",5000 );
+});
+;(function()
+{
+       $(function () {
+          $('#professional').typeahead({
+              source:proList
+          })
+      });
+})()  
+;(function()
+{
+       $('.bd_nav').find('span').click(function(){
+           $('.bd_nav').find('span').removeClass('active').eq($(this).index()).addClass('active');
+           $('.container').find('.agileits').hide().eq($(this).index()).show()
+       })
+})() 
+;(function()
+{      
+       $('.direction_close').click(function(){
+           $('.zg_direction').hide();
+           $('.zg_cover').hide();
+       })
+       $('.direction_know').click(function(){
+           $('.zg_direction').hide();
+           $('.zg_cover').hide();
+       })
+})() 
+
+
+
+function assignment(nowindex){
+   var htmls=' <option value="">工作区县</option>';
+   $.each(ds_list[nowindex-1], function(idx, obj) {
+        htmls+='<option value="'+obj+'">'+obj+'</option>' 
+   });
+   htmls+='<option value="不限">不限</option>' 
+   $('#dixian').html(htmls)
+	// if(nowindex==22){
+	//    htmls =' <option value="不限" selected>不限</option>';
+	//    $('#dixian').html(htmls)
+	//    $('#dixian').val('不限')
+	// }
+	   if(nowindex==1){
+		  htmls =' <option value="省属" selected>省属</option>';
+		  $('#dixian').html(htmls)
+		  $('#dixian').val('省属')
+	   }
+// 	if(nowindex==2){
+// 	   htmls =' <option value="不限" selected>不限</option>'; 
+
+// 	   $('#dixian').html(htmls)
+// 	   $('#dixian').val('不限')
+// }
+}
+$('#address').change(function(event) {
+   var nowIndex=$(this).get(0).selectedIndex;
+   assignment(nowIndex)
+});
+//$('.xjt').click(function(){
+//	var tlii = $(".xjt").index(this);
+//	$(".user-item").eq(tlii).find('select').click();
+//})
+
+
+
+
+
+
+
---
+++ js/searchphp.js
@@ -0,0 +1,228 @@
+// onswitch  是否开启职位不限
+$(function() {
+
+	var onswitch = false;
+	var submitswitch = false;
+	// 年份筛选
+	var year_include = function(item, target) {
+		if (item == "不限") {
+			return true
+		} else {
+			if (target == item) {
+				return true
+			} else {
+				return false
+			}
+		}
+	}
+	// 工作地点筛选 地市
+	var address_include = function(item, target) {
+		if (item == '不限') {
+			return true
+		} else {
+			if (target == item) {
+				return true
+			} else {
+				return false
+			}
+		}
+	}
+	// 工作地点筛选 地县
+	var dixian_include = function(item, target) {
+		if (item == '不限') {
+			return true
+		} else {
+			if (target.indexOf(item) !== -1) {
+				return true
+			} else {
+				return false
+			}
+		}
+	}
+	// 学历筛选
+	var school_include = function(item, target) {
+		if (item == '中专') {
+			if (target.indexOf('中专') !== -1) {
+				return true
+			} else {
+				return false
+			}
+		}
+		if (item == '大专') {
+			if (target.indexOf('中专') !== -1 || target.indexOf('大专') !== -1 || target.indexOf('专科') !== -1 ||
+				target.indexOf('普通全日制') !== -1) {
+				return true
+			} else {
+				return false
+			}
+		} else if (item == '本科') {
+			if (target.indexOf('中专') !== -1 || target.indexOf('大专') !== -1 || target.indexOf('专科') !== -1 ||
+				target.indexOf('本科') !== -1 || target.indexOf('普通全日制') !== -1) {
+				return true
+			} else {
+				return false
+			}
+		} else if (item == '研究生') {
+			return true
+		}
+	}
+	// 专业筛选
+	var professional_include = function(item, target) {
+		// 若开启专业不限按钮
+		if (onswitch) {
+			// 若未填写专业 直接判断 json数据 专业是否为专业不限
+			if (item == "") {
+				if (target == "专业不限") {
+					return true
+				} else {
+					return false
+				}
+				// 若已填写专业 直接判断 json数据的专业 是否包含已填写的专业名词 或此数据是否为专业不限
+			} else {
+				if (target.indexOf(item) !== -1 || target == "专业不限") {
+					return true
+				} else {
+					return false
+				}
+			}
+			// 若未开启专业不限按钮  则直接判断 json数据的专业 是否包含已填写的专业名词
+		} else {
+			if (item != "") {
+				if (target.indexOf(item) !== -1) {
+					return true
+				} else {
+					return false
+				}
+			}
+		}
+	}
+	var clickfn = function() {
+		var attr = [];
+		var year = $('#year').val()
+		var address = $('#address').val()
+		var dixian = $('#dixian').val()
+		var record = $('#xuel').val()
+		var professional = $('#professional').val()
+		if (year == "") {
+			alert('请选择年份!')
+			return;
+		}
+		if (address == "") {
+			alert('请选择地市!')
+			return;
+		}
+		if (dixian == "") {
+			alert('请选择区县!')
+			return;
+		}
+		if (professional == "") {
+			alert('请输入自己的专业！');
+			return false;
+		}
+		if (record == "") {
+			alert('请选择学历!')
+			return;
+		}
+		$.each(data_list, function(idx, obj) {
+			if (year_include(year, obj.item01) &&
+				address_include(address, obj.item02) &&
+				dixian_include(dixian, obj.item03) &&
+				professional_include(professional, obj.item04) &&
+				school_include(record, obj.item05)) {
+				attr.push(obj)
+			}
+		});
+	templateDo(attr)
+}
+function templateDo(jsonData) {
+	if (jsonData.length == 0) {
+		var htmls = '<p class="result_tip">没有匹配的数据,请重新修改条件查询,或者换个专业关键词查询!</p>';
+	} else {
+		var htmls = '<table class="my_table">\
+	                  <tr>\
+						  <th class="th1">地市区县</th>\
+	                      <th class="th2">专业</th>\
+						  <th class="th3">学历</th>\
+	                      <th class="th4">单位名称</th>\
+	                      <th class="th5">岗位</th>\
+	                      <th class="th6">人数</th>\
+	                      <th class="th7">其它</th>\
+	                      <th class="th8">进面比例</th>\
+	                      <th class="th9">进面最低分</th>\
+						  <th class="th10">进面最高分</th>\
+	                  </tr>';
+		for (var j in jsonData) {
+			var obj = jsonData[j];
+			htmls += '<tr>'
+			htmls += '<td>' + obj.item02 + obj.item03 +'</td>';
+			htmls += '<td>' + obj.item04 + '</td>';
+			htmls += '<td>' + obj.item05 + '</td>';
+			htmls += '<td>' + obj.item09 + '</td>';
+			htmls += '<td>' + obj.item10 + '</td>';
+			htmls += '<td>' + obj.item08 + '</td>';
+			htmls += '<td>' + obj.item19 + '</td>';
+			htmls += '<td>' + obj.item16 + '</td>';
+			htmls += '<td>' + obj.item17 + '</td>';
+			htmls += '<td>' + obj.item18 + '</td>';
+			htmls += '</tr>'
+		}
+		htmls += '</table>';
+	}
+	$('.homePage').addClass('active')
+	$('.table_wrap').html('').append(htmls);
+}
+$('#submit').click(function() {
+	clickfn()
+})
+
+
+// $('#submit').click(function() {
+// 	if ($.cookie('cookiePhone')) {
+// 		clickfn()
+// 	} else {
+// 		alert("请先注册或登录");
+// 		$('.zg_cover').show();
+// 		$('.container').show();
+// 	}
+// })
+
+// $('.login_btn').click(function() {
+// 	if (submitswitch) {
+// 		alert('您已登陆!')
+// 	} else {
+// 		$('.zg_cover').show();
+// 		$('.container').show();
+// 		$('.bd_nav').find('span').removeClass('active').eq(0).addClass('active');
+// 		$('.container').find('.agileits').hide().eq(0).show();
+// 	}
+// }) 
+// $('.register_btn').click(function() {
+// 	if (submitswitch) {
+// 		alert('您已登陆!')
+// 	} else {
+// 		$('.zg_cover').show();
+// 		$('.container').show();
+// 		$('.bd_nav').find('span').removeClass('active').eq(1).addClass('active');
+// 		$('.container').find('.agileits').hide().eq(1).show();
+// 	}
+// })
+
+//点击专业不限按钮  切换 onswitch 布尔值  并触发查询函数
+$(".item_check").click(function() {
+	//  if(submitswitch){
+	$(this).toggleClass('active');
+	onswitch = !onswitch;
+	clickfn()
+	//  }else{
+	//    alert("请先注册或登录");
+	//    $('.zg_cover').show();
+	//    $('.container').show();
+	//  }
+	//  
+})
+
+$('.bd_close').click(function() {
+	$('.zg_cover').hide();
+	$('.container').hide();
+})
+})
